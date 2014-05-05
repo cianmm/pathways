ed77d90bbec0594543219ecd7b3cc7ae39119f5e
Cian Mac Mahon <cianmm@gmail.com>
2014-05-05 12:56:02 +0100

    Fixed division by 0
    Fixed an instance where you could end up dividing by zero, and removed
    the ‘0’ placed on the progress chart if 0% of a goal has been completed.

--- app/models/Goal.php
+++ app/models/Goal.php
@@ -5,7 +5,16 @@ class Goal extends Eloquent {
 
     public static function getCurrent($value, $complete)
     {
-        return ($value / $complete) * 100;
+        // first let's check if we are going to devide by 0
+        // and if so, let's just return 0. 
+        if ($value == 0)
+        {
+            return 0;
+        }
+        elseif ($value > 0 )
+        {
+            return ($value / $complete) * 100;
+        }
     }
 
     
--- app/views/goals/show.blade.php
+++ app/views/goals/show.blade.php
@@ -3,11 +3,20 @@
 @section('content')
 
     <h1>{{$goal->goal_title}}</h1>
+    
     <p>{{$goal->goal_description}}</p>
+    
     <div class="progress">
+    
         <div class="progress-bar" role="progressbar" aria-valuenow="{{$goalCurrent}}" aria-valuemin="0" aria-valuemax="100" style="width: {{$goalCurrent}}%;">
-            {{ number_format($goalCurrent, 0) }}%
+            @if ($goalCurrent > 0 )        
+                {{ number_format($goalCurrent, 0) }}%
+            @endif
+            
         </div>
+        
         </div>
+
     <p>
+
 @stop
