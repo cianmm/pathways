2fed6a460cbf22c286a5f6459fb2a3640f563f26
Cian Mac Mahon <cianmm@gmail.com>
2014-05-07 22:28:10 +0100

    Refactoring of navbar!
    Navbar is now cleaner, code wise, in it’s own file.

---
+++ app/views/includes/navbar.blade.php
@@ -0,0 +1,33 @@
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+			        <span class="sr-only">Toggle login</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+				</button>
+
+	            <a class="navbar-brand" href="/goals">Pathways</a>
+			</div>
+			@if (Auth::guest())
+				<div class="collapse navbar-collapse" id="navbar-collapse-1">
+					{{ Form::open(array('route' => 'sessions.store', 'role'=>'form', 'class'=>'navbar-form navbar-right'))}}
+					  <div class="form-group">	        
+						  {{ Form::input('text', 'username', null, array('class'=>'form-control', 'placeholder'=>'Username'))}}
+					  </div>
+					  <div class="form-group">	        
+						  {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password'))}}
+					  </div>
+					  <div class="form-group">	        
+					  	  {{ Form::submit('Log in', array('class'=>'btn btn-success')) }}
+					  </div>
+					{{ Form::close()}}
+				</div>
+			@else
+				<div>
+					<a href="logout" type="button" class="btn btn-danger navbar-btn navbar-right">Log Out</a>
+				</div>
+			@endif
+        </div>
+    </nav>
